962f7379c7c1baf2d91d7db2c8d6909a60be4614
Ali <ali.najafpour07@gmail.com>
2026-01-01 18:21:34 +0330

    Update Main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from google import genai
 import asyncio
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 
 
@@ -11,13 +11,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I am better promt bot Send your promt so i can make it better")
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 TOKEN = "8286997548:AAGNAVERznnt7UpIYT01cvIlBq-NY_8ssqc"
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
 
     start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
     application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+
     application.run_polling()
 
 
